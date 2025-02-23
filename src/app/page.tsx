43f69fef95d7b072e71621ce245e5f45f713365b
Sampurna <sampurng@gmail.com>
2025-02-23 03:33:03 -0500

    Added Safety Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Form } from "./Components/Form";
 import { Header } from "./Components/Header";
+import { Safe } from "./Components/Safe";
 
 export default function Home() {
   return (
     <div className="bg-[#262626]">
       <Header />
       <Form />
+      <Safe />
     </div>
   );
 }
